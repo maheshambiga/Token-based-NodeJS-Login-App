d85b209e4aa45f53a4f758b654b7cb18a3078582
Mahesh Ambig <mahesh.ambiga@gmail.com>
2016-05-12 12:16:08 +0530

    modified server session time-out duration

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ var config = {
   "PORT":"3000",
   "TOKEN_SECRET": "thisisasecretkey",
   "DATABASE_CONNECTION" : "mongodb://localhost:27017/mydb",
-  "TOKEN_EXPIRY_TIME" : 2//mins
+  "TOKEN_EXPIRY_TIME" : 15//mins
 }
 
 module.exports = config;
